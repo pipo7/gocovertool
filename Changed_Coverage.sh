028d1daf4f2b9898b730b14a69f777f013aa6ea2
psinghal <p.singhal@f5.com>
2023-03-20 09:05:28 +0000

    updated steps for differentail test coverage in Go

--- Changed_Coverage.sh
+++ Changed_Coverage.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash 
 
 # Assumes the filename is cover.out
-# 
+# mode is SET " checks that each statement ran"
 go test ./... -covermode=set -coverprofile cover.out
 
 # Sets up new_coverage.out file
 echo "mode: set" > new_coverage.out 
+# falgs used :
+# --no-ext-diff Disallow external diff drivers.
+# --unified=<n> Generate diffs with <n> lines of context instead of the usual three. Implies --patch
+# --no-prefix Do not show any source or destination prefix.
+# -a treat all files as text
+# --exit-code Make the program exit with codes similar to diff(1). That is, it exits with 1 if there were differences and 0 means no differences.
 git diff HEAD --no-ext-diff --unified=0 --exit-code -a --no-prefix cover.out | egrep "^\+" | tail -n +2 | cut -c2- >> new_coverage.out 
 
 # Generate new_coverage report
