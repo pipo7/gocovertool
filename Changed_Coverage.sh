1c4b28ca3b72c491dbdc44ec16c5ac22ac939ffe
psinghal <p.singhal@f5.com>
2023-03-24 09:37:29 +0000

    changed filename to cp.out from cover.out

--- Changed_Coverage.sh
+++ Changed_Coverage.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash 
 
 # delete any existng file
-rm new_coverage*
+rm -f new_coverage*
 
-# Assumes the filename is cover.out
+# Assumes the filename is cp.out
 # mode is SET " checks that each statement ran"
-go test ./... -covermode=set -coverprofile cover.out
+go test ./... -covermode=set -coverprofile cp.out
 
-# Sets up new_coverage.out file
-echo "mode: set" > new_coverage.out 
-# falgs used :
+
+# falgs used to get difference of existing checked-in cp.out and changed cp.out:
 # --no-ext-diff Disallow external diff drivers.
 # --unified=<n> Generate diffs with <n> lines of context instead of the usual three. Implies --patch
 # --no-prefix Do not show any source or destination prefix.
 # -a treat all files as text
 # --exit-code Make the program exit with codes similar to diff(1). That is, it exits with 1 if there were differences and 0 means no differences.
-coverFileDiff=$(git diff HEAD --no-ext-diff --unified=0 --exit-code -a --no-prefix cover.out | egrep "^\+" | tail -n +2 | cut -c2-)
+coverFileDiff=$(git diff HEAD --no-ext-diff --unified=0 --exit-code -a --no-prefix cp.out | egrep "^\+" | tail -n +2 | cut -c2-)
 
 if [ -z "$coverFileDiff" ]
 then
-  echo "No change in coverage"
+  echo "No NEW change in code coverage"
   FILE="coverage.txt"
   if [ -f $FILE ]; then
     TOTAL_COVERAGE=$(egrep '^total:.*(statements).*' $FILE | egrep -o '[0-9]+\.[0-9]+')
@@ -27,7 +26,9 @@ then
   fi
 else
   echo "coverFileDiff is NOT empty thus creating new_coverage.txt file"
-  git diff HEAD --no-ext-diff --unified=0 --exit-code -a --no-prefix cover.out | egrep "^\+" | tail -n +2 | cut -c2- >> new_coverage.out 
+  # Sets up new_coverage.out file
+  echo "mode: set" > new_coverage.out 
+  git diff HEAD --no-ext-diff --unified=0 --exit-code -a --no-prefix cp.out | egrep "^\+" | tail -n +2 | cut -c2- >> new_coverage.out 
   # Generate new_coverage report
   go tool cover -func new_coverage.out | tee new_coverage.txt 
   # New Coverage html file
@@ -35,7 +36,7 @@ else
   FILE="new_coverage.txt"
   if [ -f $FILE ]; then
     TOTAL_COVERAGE=$(egrep '^total:.*(statements).*' $FILE | egrep -o '[0-9]+\.[0-9]+')
-    echo "NEW coverage % is: $TOTAL_COVERAGE%"
+    echo "Code coverage for new features is: $TOTAL_COVERAGE%"
   fi
 fi
 
